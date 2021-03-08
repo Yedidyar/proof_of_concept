33236665954064fb2ee05a1e3b8f41f090552944
yedidyar <yedidyarashi@gmail.com>
2021-03-08 19:38:49 +0200

    update

--- proof_of_concept/main.py
+++ proof_of_concept/main.py
@@ -1,8 +1,15 @@
-from flask import Flask,request, render_template
+from flask import Flask, request, render_template
+from solver import trajectory_calculator
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
+    if request.method == 'GET':
+        velocity = request.values.get('velocity', type=int)
+        angle = request.values.get('angel', type=int)
+        h, total_length, total_time = trajectory_calculator(velocity,angle)
     return render_template('index.html')
 
 
