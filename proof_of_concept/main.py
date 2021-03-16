5cd77a079321ee5905c487bf8c481e219cc4db98
yedidyar <yedidyarashi@gmail.com>
2021-03-16 11:51:41 +0200

    Submit AJAX Forms with jQuery to Flask

--- proof_of_concept/main.py
+++ proof_of_concept/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from solver import trajectory_calculator
 app = Flask(__name__)
 
@@ -10,17 +10,29 @@ def index():
 
 @app.route('/', methods=['POST'])
 def calculate():
-    error = None
-    if request.method == 'POST':
-        if request.form['velocity'] and request.form['angle']:
-            velocity = request.values.get('velocity', type=float)
-            angle = request.values.get('angle', type=float)
-            h, total_length, total_time = trajectory_calculator(
-                velocity, angle)
-            return render_template('index.html', h=h, total_length=total_length, total_time=total_time, error=error)
-        else:
-            error = 'the input must be an integer or a float number.'
-            return render_template('index.html', error=error)
+    # error = None
+    # if request.method == 'POST':
+    #     if request.form['velocity'] and request.form['angle']:
+    #         velocity = request.values.get('velocity', type=float)
+    #         angle = request.values.get('angle', type=float)
+    #         h, total_length, total_time = trajectory_calculator(
+    #             velocity, angle)
+    #         return render_template('index.html', h=h, total_length=total_length, total_time=total_time, error=error)
+    #     else:
+    #         error = 'the input must be an integer or a float number.'
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    velocity = request.form['velocity']
+    angle = request.form['angle']
+
+    if angle.isnumeric() and velocity.isnumeric():
+        h, total_length, total_time = trajectory_calculator(
+            float(velocity), float(angle))
+        return jsonify({'h': h, 'total_length': total_length, 'total_time': total_time})
+    return jsonify({'error': 'the input must be an integer or a float number.'})
 
 
 if __name__ == '__main__':
--- proof_of_concept/static/js/toggel_table.js
+++ proof_of_concept/static/js/toggel_table.js
@@ -1,13 +1,24 @@
-//  let button = document.getElementById("submit_button")
-//  button.onclick = function (){
-
-//     let x = document.getElementById("output");
-//     x.style.display= "block";
-// }
-
-let toggle_visibility = function () {
-   
-    let button = document.getElementById("output");
-    button.style.display = "block" ;
-  return false;
-};
+$(document).ready(function () {
+  $("form").on("submit", function (event) {
+    $.ajax({
+      data: {
+        velocity: $("#velocity").val(),
+        angle: $("#angle").val(),
+      },
+      type: "POST",
+      url: "/process",
+    }).done(function (data) {
+      if (data.error) {
+        $("#error").text(data.error).show();
+        $("#result").hide();
+      } else {
+        $("#h").text(data.h).show();
+        $("#total_length").text(data.total_length).show();
+        $("#total_time").text(data.total_time).show();
+        $("#result").show();
+        $("#error").hide();
+      }
+    });
+    event.preventDefault();
+  });
+});
